c8ed18d334b5c3670b1d2cf75a723f3d8b3a1322
VictorCali1702 <wiktor.naczk199@gmail.com>
2026-06-04 18:03:18 +0200

    Fitness tracker: forms and BMI calculator some bugs fixed and list of histpry of trainings

--- index.php
+++ index.php
@@ -1,3 +1,65 @@
+<?php
+
+session_start();
+
+$wynik_treningu = "";
+$wynik_bmi = "";
+
+if (isset($_POST["Zapisz"])){
+    $rodzaj_treningu = $_POST["rodzaj"];
+    $liczba_cwiczen = $_POST["liczba_cwiczen"];
+    $czas_treningu = $_POST["czas"];
+
+    $wynik_treningu = "Trening: {$rodzaj_treningu} zawiera {$liczba_cwiczen} ćwiczeń. Trening o: {$czas_treningu}";
+
+    if (!isset($_SESSION["treningi"])){
+        $_SESSION["treningi"] = [];
+    }
+
+    $_SESSION["treningi"][] = $wynik_treningu;
+    
+}
+    
+// Kalkulator BMI
+// Wzrost i waga
+
+if (isset($_POST["bmi"])){
+
+    $wzrost = $_POST["wzrost"];
+    $waga = $_POST["waga"];
+
+    if($wzrost > 0 && $waga > 0){
+        $converted_wzrost = $wzrost / 100;
+
+        $bmi = $waga / ($converted_wzrost ** 2);
+        $bmi_rounded = round($bmi, 1);
+        $wynik_bmi = "BMI: {$bmi_rounded}";
+
+        switch(true) {
+            case ($bmi_rounded < 18.5):
+                $category = "Niedowaga";
+                break;
+            case ($bmi_rounded <= 24.9):
+                $category = "Normalna waga";
+                break;
+            case ($bmi_rounded <= 29.9):
+                $category = "Nadwaga";
+                break;
+            default:
+                $category = "Otyłość";
+        }
+        
+        $wynik_bmi = "Twoje BMI wynosi: {$bmi_rounded}<br>Kategoria: {$category}";
+    }
+
+}
+
+
+
+
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -18,7 +80,7 @@
                 <h2>Witaj!</h2>
                 <p>Śledź swoje treningi i postępy.</p>
                 <form action=index.php method="post">
-                    <label>Rodzaj treningu: </labe>
+                    <label>Rodzaj treningu: </label>
                     <input type="text" name="rodzaj"></br>
                     <label>Liczba ćwiczeń: </label>
                     <input type="number" name="liczba_cwiczen"></br>
@@ -26,7 +88,9 @@
                     <input type="time" name="czas"></br>
                     <input type="submit" name="Zapisz" value="Zapisz trening"></br>
                 </form>
-                
+                <div class="result">
+                    <?php echo $wynik_treningu; ?>
+                </div>
                 <h3>Kalkulator BMI</h3>
                 <p>Sprawdź czy twoje BMI jest dobre</br>
                 Podaj swój wzrost oraz wagę ciała</p></br>
@@ -37,9 +101,25 @@
                     <input type="number" name="waga">KG</br>
                     <input type="submit" name="bmi" value="Oblicz"></br>
                 </form>
+                <div class="result">
+                    <?php echo $wynik_bmi; ?>
+                </div>
             </div>
             
             <div class="right">
+                <div class="treningi">
+                    <h2>Ostatnie treningi</h2>
+                    <?php 
+                    if (isset($_SESSION["treningi"])){
+                        echo "<p>Liczba treningów: " . count($_SESSION["treningi"]) . "</p>";
+                        
+                        foreach($_SESSION["treningi"] as $trening){
+                            echo "<p>$trening</p>";
+                        }
+                    }
+                    ?>
+
+                </div>
             </div>
         
         </div>
@@ -50,46 +130,3 @@
 </body>
 </html>
 
-<?php
-
-$wynik_treningu = "";
-$wynik_bmi = "";
-
-if (isset($_POST["Zapisz"])){
-    $rodzaj_treningu = $_POST["rodzaj"];
-    $liczba_cwiczen = $_POST["liczba_cwiczen"];
-    $czas_treningu = $_POST["czas"];
-
-    $wynik_treningu = "Trening: {$rodzaj_treningu} zawiera {$liczba_cwiczen} ćwiczeń. Trening o: {$czas_treningu}";
-}
-    
-// Kalkulator BMI
-// Wzrost i waga
-
-if (isset($_POST["bmi"])){
-    $wzrost = $_POST["wzrost"];
-    $waga = $_POST["waga"];
-
-    $converted_wzrost = $wzrost / 100;
-
-    $bmi = $waga / ($converted_bmi ** 2);
-    $bmi_rounded = round($bmi, 1);
-
-    switch(true) {
-        case ($bmi_rounded < 18.5):
-            $category = "Niedowaga";
-            break;
-        case ($bmi_rounded <= 24.9):
-            $category = "Normalna waga";
-            break;
-        case ($bmi_rounded <= 29.9):
-            $category = "Nadwaga";
-            break;
-        default:
-            $category = "Otyłość";
-    }
-    $wynik_bmi = "Twoje BMI wynosi: {$bmi_rounded}<br>Kategoria: ($category)";
-}
-
-
-?>
